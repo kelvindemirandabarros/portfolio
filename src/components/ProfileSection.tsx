426a09e56bcd41083ae392b58b958d9565f97ea9
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2024-04-25 01:45:51 -0300

    fix: Correção de font-size
    - Removido @media font-size do título h1 por ficar grande demais entre telas 640px e 1024px.

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -10,7 +10,7 @@ export function ProfileSection() {
     <section className='lg:py-16'>
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
-          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600'>{`Olá, Kelvin aqui...`}</span>
           </h1>
 
