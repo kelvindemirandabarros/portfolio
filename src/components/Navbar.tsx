9ecd49083b3accce58b240476c93fa818365f68e
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2024-04-26 18:54:21 -0300

    feat: Melhorando NavBar
    - Uso do componente NavLink para os botões do menu.
    - Adição de botões de abrir e fechar menu em telas pequenas.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+
+// Components:
+import { NavLink } from './NavLink';
+
+// Consts:
+const nav_links = [
+  {
+    title: 'Sobre',
+    href: '#about'
+  },
+  {
+    title: 'Projeto',
+    href: '#project'
+  },
+  {
+    title: 'Contato',
+    href: '#contact'
+  }
+];
 
 export function Navbar() {
+  const [navbar_open, set_navbar_open] = useState(false);
+
   return (
-    <nav>
-      <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-        <Link href={'/'} className='text-5xl text-white font-semibold'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+      <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
+        <Link
+          href={'/'}
+          className='text-2xl md:text-5xl text-white font-semibold'
+        >
           Logo
         </Link>
 
-        <div className='menu hidden md:block md:w-auto' id='navbar'>
-          <ul>
-            <li>
-              <Link
-                href={'#about'}
-                className='block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounder md:p-0 hover:text-white'
-              >
-                About
-              </Link>
-            </li>
+        <div className='mobile-menu block md:hidden'>
+          {!navbar_open ? (
+            <button
+              onClick={() => set_navbar_open(true)}
+              className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+            >
+              <Bars3Icon className='h-5 w-5' />
+            </button>
+          ) : (
+            <button
+              onClick={() => set_navbar_open(false)}
+              className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+            >
+              <XMarkIcon className='h-5 w-5' />
+            </button>
+          )}
+        </div>
+
+        <div className='menu hidden md:w-auto' id='navbar'>
+          <ul className='flex p-4 md:p-0  md:flex-row md:space-x-8 mt-0'>
+            {nav_links.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.href} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
