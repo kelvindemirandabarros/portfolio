47e929039131b28c61728d987656074470d0e414
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2024-04-29 20:55:40 -0300

    feat: Adição de animações na seção de perfil.

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -1,6 +1,8 @@
 'use client';
+
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 // Components:
 import { TypeAnimation } from 'react-type-animation';
@@ -11,7 +13,12 @@ export function ProfileSection() {
   return (
     <section className='lg:py-16'>
       <div className='grid grid-cols-1 sm:grid-cols-12'>
-        <div className='col-span-7 place-self-center text-center sm:text-left'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.4 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className='col-span-7 place-self-center text-center sm:text-left'
+        >
           <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600'>{`Olá, Kelvin aqui...`}</span>
           </h1>
@@ -60,9 +67,14 @@ export function ProfileSection() {
               </span>
             </button>
           </div>
-        </div>
+        </motion.div>
 
-        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.4 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className='col-span-5 place-self-center mt-4 lg:mt-0'
+        >
           <div className='rounded-full bg-[#181818] w-[200px] h-[200px] lg:w-[200px] lg:h-[200px] relative'>
             <Image
               src='https://github.com/kelvindemirandabarros.png'
@@ -72,7 +84,7 @@ export function ProfileSection() {
               className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full'
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
