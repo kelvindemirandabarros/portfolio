2672ae4fb52289d28c9d3a7851c43e5a2521a0eb
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2024-05-07 01:31:41 -0300

    feat: Adicionado link para o botão currículo.

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -63,11 +63,16 @@ export function ProfileSection() {
               LinkedIn
             </button>
 
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-purple-500 hover:bg-slate-800 text-white mt-3'>
-              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
-                Baixar Currículo
-              </span>
-            </button>
+            <a
+              href='https://programathor.s3.sa-east-1.amazonaws.com/uploads/user/curriculum/54952/curriculo_kelvin_de_miranda_barros.pdf?X-Amz-Expires=600&X-Amz-Date=20240507T041618Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIIL73EXGLO35ED5Q%2F20240507%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=58b03ecf09688d44f8a2dfcb0ce1ced9c10b1ff1f8027073859440d1aad0dc0e'
+              target='_blank'
+            >
+              <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-purple-500 hover:bg-slate-800 text-white mt-3'>
+                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                  Baixar Currículo
+                </span>
+              </button>
+            </a>
           </div>
         </motion.div>
 
