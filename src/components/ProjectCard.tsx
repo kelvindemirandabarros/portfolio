20039b0c46072aa15330a894f1353ad5a5eb5c87
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2026-07-10 14:52:42 -0300

    fix: Corrigido problema que smartphones não conseguiam navegar para a página do projeto.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,18 +15,13 @@ export function ProjectCard({
   page_url
 }: ProjectCardInterface) {
   return (
-    <div>
+    <Link href={page_url} className='block group'>
       <div
         className='h-52 md:h-72 rounded-t-xl relative group'
         style={{ background: `url(${img_url})`, backgroundSize: 'cover' }}
       >
         <div className='items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
-          <Link
-            href={page_url}
-            className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
-          >
-            <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
-          </Link>
+          <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white' />
         </div>
       </div>
 
@@ -35,6 +30,6 @@ export function ProjectCard({
 
         <p className='text-[#ADB7BE]'>{description}</p>
       </div>
-    </div>
+    </Link>
   );
 }
